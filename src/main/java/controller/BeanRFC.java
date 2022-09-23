1ccfd1481bd8af45ea51d8cd15c5052155b67d7b
HernandezBrandon <113206518+HernandezBrandon@users.noreply.github.com>
2022-09-22 22:55:27 -0500

    Update BeanRFC.java

--- src/main/java/controller/BeanRFC.java
+++ src/main/java/controller/BeanRFC.java
@@ -24,48 +24,54 @@ public BeanRFC(int id,String nombre,String apellido1,String apellido2,String cur
         return rfc;
     }
 
-    public void setRfc(String rfc) {
+    public String setRfc(String rfc) {
         this.rfc = rfc;
+        return rfc;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public String setNombre(String nombre) {
         this.nombre = nombre;
+        return nombre;
     }
 
     public String getApellido1() {
         return apellido1;
     }
 
-    public void setApellido1(String apellido1) {
+    public String setApellido1(String apellido1) {
         this.apellido1 = apellido1;
+        return apellido1;
     }
 
     public String getApellido2() {
         return apellido2;
     }
 
-    public void setApellido2(String apellido2) {
+    public String setApellido2(String apellido2) {
         this.apellido2 = apellido2;
+        return apellido2;
     }
 
     public String getCurp() {
         return curp;
     }
 
-    public void setCurp(String curp) {
+    public String setCurp(String curp) {
         this.curp = curp;
+        return curp;
     }
 
     public String getFecha() {
         return fecha;
     }
 
-    public void setFecha(String fecha) {
+    public String setFecha(String fecha) {
         this.fecha = fecha;
+        return fecha;
     }
 
     public int getId() {
